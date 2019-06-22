35338f294cbc3df96614634e71a3fe9f63c8a205
Vladimir Klimov <vladimirklimoff@yandex.ru>
2019-06-22 22:54:47 +0500

    Updates

---
+++ precache-manifest.1bd8d4004935b57bf0583019725d8eb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d722ed60c3a05bff810dd5cbf3f504fa",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4d4662b11c2c669a73d6",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7c249940077ed29d8d7e",
+    "url": "/robofriends/static/css/main.cee58a45.chunk.css"
+  },
+  {
+    "revision": "4d4662b11c2c669a73d6",
+    "url": "/robofriends/static/js/2.2b3ff20d.chunk.js"
+  },
+  {
+    "revision": "7c249940077ed29d8d7e",
+    "url": "/robofriends/static/js/main.05ddcc57.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
