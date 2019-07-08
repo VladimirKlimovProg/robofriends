b84c0f8a476b8d193c601770d8655bd9e3da8168
Vladimir Klimov <vladimirklimoff@yandex.ru>
2019-07-08 16:08:59 +0500

    add more test

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const SearchBox = ({ searchChange }) => {
-	// console.log('searchbox')
-	return (
-		<div className='pa2'>
-			<input
-				aria-label='Search Robots'
-				className='pa3 ba b--green bg-lightest-blue'
-				type='search'
-				placeholder='search robots'
-				onChange={searchChange}
-			/>
-		</div>
-	);
-
-}
+  // console.log('searchbox')
+  return (
+    <div className="pa2">
+      <input
+        aria-label="Search Robots"
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
